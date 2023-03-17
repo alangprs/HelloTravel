c1ffe3abf181d825f37d6e13ecc4c896282e22b0
Will_Weng <alangprs1@gmail.com>
2023-03-17 11:28:24 +0800

    feat:  加入 NearbyLandmarkVC TopView

--- HelloTravel/ViewController/NearbyLandmarkVC/NearbyLandmarkVC.swift
+++ HelloTravel/ViewController/NearbyLandmarkVC/NearbyLandmarkVC.swift
@@ -17,10 +17,69 @@ class NearbyLandmarkVC: UIViewController {
         return vm
     }()
 
+    // MARK: - 最上方區域
+    private lazy var topView: UIView = {
+        var view = UIView()
+
+        return view
+    }()
+
+    private lazy var bgImage: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "KabdnarkTop")
+        return imageView
+    }()
+
+    private lazy var topTitleLabel: UILabel = {
+        var label = UILabel()
+        label.textColor = .black
+        label.text = "你周圍的餐廳、景點"
+        return label
+    }()
+
+    // MARK: -
+
+    // MARK: - 生命週期
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        viewModel.askPermission()
+        setupUI()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+//        viewModel.askPermission()
+    }
+
+    // MARK: - 其他
+
+    private func setupUI() {
+        setupTopView()
+    }
+
+    private func setupTopView() {
+        view.addSubview(topView)
+        topView.backgroundColor = .systemRed
+        topView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(280)
+        }
+
+        topView.addSubview(bgImage)
+        bgImage.backgroundColor = .systemGray
+
+        topView.addSubview(topTitleLabel)
+
+        bgImage.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+
+        topTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.leading.equalToSuperview().offset(12)
+        }
     }
 
     /// 未開啟定位通知
@@ -35,7 +94,7 @@ class NearbyLandmarkVC: UIViewController {
     }
 }
 
-// MARK: -
+// MARK: - NearbyLandmarkVMDelegate
 
 extension NearbyLandmarkVC: NearbyLandmarkVMDelegate {
     func noGPSPermission() {
