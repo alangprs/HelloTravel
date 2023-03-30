9196017943bc0c003207a4e9982b2b4b4006f3e6
Will_Weng <alangprs1@gmail.com>
2023-03-30 17:59:33 +0800

    feat: 加入評論數、星星數 UI

--- HelloTravel/ViewController/BusinessSheetVC/SheetTableViewCell.swift
+++ HelloTravel/ViewController/BusinessSheetVC/SheetTableViewCell.swift
@@ -29,7 +29,17 @@ class SheetTableViewCell: UITableViewCell {
         imageView.backgroundColor = .green
         return imageView
     }()
-    
+
+    /// 顯示星星數量文字
+    private lazy var starsCountLabel: UILabel = {
+        var label = UILabel()
+        return label
+    }()
+
+    /// 圖片下載器
+    private lazy var sdWebImageAdapter: SDWebImageAdapter = {
+        return SDWebImageAdapter()
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -47,24 +57,31 @@ class SheetTableViewCell: UITableViewCell {
         contentView.addSubview(imageView01)
         contentView.addSubview(titleLabel)
         contentView.addSubview(starsCountImageView)
+        contentView.addSubview(starsCountLabel)
 
         imageView01.snp.makeConstraints { make in
             make.top.leading.equalToSuperview().inset(12)
             make.width.height.equalTo(100)
             make.bottom.equalTo(titleLabel.snp.top).inset(-6)
         }
-        
+
         titleLabel.text = "咖啡吧"
         titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(12)
+        }
+
+        starsCountLabel.text = "5656"
+        starsCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
             make.leading.equalToSuperview().inset(12)
             make.bottom.equalToSuperview().offset(-6)
         }
 
         starsCountImageView.snp.makeConstraints { make in
-            make.leading.equalTo(titleLabel.snp.trailing).inset(-6)
+            make.leading.equalTo(starsCountLabel.snp.trailing).inset(-6)
             make.width.equalToSuperview().multipliedBy(0.3)
             make.height.equalTo(titleLabel.snp.height)
-            make.centerY.equalTo(titleLabel.snp.centerY)
+            make.centerY.equalTo(starsCountLabel.snp.centerY)
         }
 
     }
