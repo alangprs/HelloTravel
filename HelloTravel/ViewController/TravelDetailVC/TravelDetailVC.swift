bfdb44c1063ebe861af3a12109797900984f7b58
Will_Weng <alangprs1@gmail.com>
2023-04-10 11:38:37 +0800

    feat: 下拉放大 headerView

--- HelloTravel/ViewController/TravelDetailVC/TravelDetailVC.swift
+++ HelloTravel/ViewController/TravelDetailVC/TravelDetailVC.swift
@@ -14,14 +14,43 @@ class TravelDetailVC: UIViewController {
         var tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.addSubview(headerImageView)
+        tableView.contentInset = UIEdgeInsets(top: headerHeight, left: 0, bottom: 0, right: 0)
 //        tableView.register(SheetTableViewCell.self, forCellReuseIdentifier: "\(SheetTableViewCell.self)")
         return tableView
+    }()
+
+    private lazy var headerImageView: UIImageView = {
+        var headerView = UIImageView()
+        headerView.frame = CGRect.init(x: 0, y: -headerHeight, width: view.frame.width, height: headerHeight)
+        headerView.contentMode = .scaleAspectFill
+        headerView.clipsToBounds = true
+        headerView.image = UIImage(named: "testImage")
+        return headerView
+    }()
+
+    /// 預設 header 高度
+    private lazy var headerHeight: CGFloat = {
+        var height = 230.0
+        return height
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
     private func setupUI() {
@@ -45,5 +74,20 @@ extension TravelDetailVC: UITableViewDelegate, UITableViewDataSource {
         return UITableViewCell()
     }
 
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
 
+        return headerHeight
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let radius = -offsetY/headerHeight
+
+        if (-offsetY > headerHeight) {
+            headerImageView.transform = CGAffineTransform.init(scaleX: radius, y: radius)
+            var frame = headerImageView.frame
+            frame.origin.y = offsetY
+            headerImageView.frame = frame
+        }
+    }
 }
