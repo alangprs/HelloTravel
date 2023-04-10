e01095fc87711a50309cb70d94e209c623dc76de
Will_Weng <alangprs1@gmail.com>
2023-04-10 14:46:57 +0800

    add: 抬頭、星星圖片、星星數量文字、更多圖片按鈕

--- HelloTravel/ViewController/TravelDetailVC/TravelDetailVC.swift
+++ HelloTravel/ViewController/TravelDetailVC/TravelDetailVC.swift
@@ -74,6 +74,42 @@ class TravelDetailVC: UIViewController {
         return height
     }()
 
+    private lazy var titleLabel: UILabel = {
+        var label = UILabel()
+        label.text = "測試抬頭"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+
+    /// 依照星星數量決定圖片
+    private lazy var starsCountImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.backgroundColor = .green
+        return imageView
+    }()
+
+    /// 顯示星星數量文字
+    private lazy var starsCountLabel: UILabel = {
+        var label = UILabel()
+        label.text = "56"
+        label.textColor = .white
+        return label
+    }()
+
+    /// 更多圖片按鈕
+    private lazy var moreImageButton: UIButton = {
+        var btn = UIButton()
+        btn.setTitle(" 觀看更多圖片 ", for: .normal)
+        btn.titleLabel?.font = .systemFont(ofSize: 12)
+        btn.titleLabel?.textColor = .white
+        btn.backgroundColor = .brown.withAlphaComponent(0.5)
+        btn.contentEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        btn.layer.cornerRadius = 5
+        btn.addTarget(self, action: #selector(didClickMoreImageButton), for: .touchUpInside)
+        return btn
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -98,6 +134,11 @@ class TravelDetailVC: UIViewController {
         headerImageView.addSubview(moreButton)
         headerImageView.addSubview(shareButton)
         headerImageView.addSubview(likeButton)
+        headerImageView.addSubview(titleLabel)
+        headerImageView.addSubview(starsCountImageView)
+        headerImageView.addSubview(starsCountLabel)
+        headerImageView.addSubview(titleLabel)
+        headerImageView.addSubview(moreImageButton)
 
         detailTableView.snp.makeConstraints { make in
             make.top.bottom.leading.trailing.equalToSuperview()
@@ -122,6 +163,28 @@ class TravelDetailVC: UIViewController {
             make.top.equalToSuperview().offset(30)
             make.trailing.equalTo(shareButton.snp.leading).offset(-20)
         }
+
+        starsCountImageView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-20)
+            make.leading.equalToSuperview().offset(30)
+            make.width.equalToSuperview().multipliedBy(0.3)
+            make.height.equalTo(25)
+        }
+
+        starsCountLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-20)
+            make.leading.equalTo(starsCountImageView.snp.trailing).offset(10)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(starsCountImageView.snp.top).offset(-12)
+            make.leading.equalToSuperview().offset(30)
+        }
+
+        moreImageButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-20)
+            make.trailing.equalToSuperview().offset(-40)
+        }
     }
 
     // MARK: - action
@@ -144,6 +207,11 @@ class TravelDetailVC: UIViewController {
     /// 喜歡按鈕
     @objc private func didClickLikeButton() {
         // TODO: 喜歡按鈕動作
+    }
+
+    /// 更多圖片按鈕
+    @objc private func didClickMoreImageButton() {
+        // TODO: 更多圖片按鈕動作
     }
 }
 
