393cfd026b4bbc2edb104afa313cc56d354b98f6
Will_Weng <Will_Weng@cmoney.com.tw>
2023-04-14 12:46:26 +0800

    style: 排版整理

--- HelloTravel/ViewController/TravelDetailVC/TravelDetailVC.swift
+++ HelloTravel/ViewController/TravelDetailVC/TravelDetailVC.swift
@@ -9,11 +9,11 @@ import UIKit
 import SnapKit
 
 class TravelDetailVC: UIViewController {
-
+    
     private var viewModel: TravelDetailVM?
-
+    
     // MARK: - UI 元件
-
+    
     private lazy var detailTableView: UITableView = {
         var tableView = UITableView()
         tableView.delegate = self
@@ -26,7 +26,7 @@ class TravelDetailVC: UIViewController {
         registerCell(tableView: tableView)
         return tableView
     }()
-
+    
     private lazy var headerImageView: UIImageView = {
         var headerView = UIImageView()
         headerView.frame = CGRect.init(x: 0, y: -headerHeight, width: view.frame.width, height: headerHeight)
@@ -36,7 +36,7 @@ class TravelDetailVC: UIViewController {
         headerView.isUserInteractionEnabled = true
         return headerView
     }()
-
+    
     /// 返回按鈕
     private lazy var backButton: UIButton = {
         var btn = UIButton()
@@ -48,7 +48,7 @@ class TravelDetailVC: UIViewController {
         btn.addTarget(self, action: #selector(didClickBackButton), for: .touchUpInside)
         return btn
     }()
-
+    
     /// 喜歡按鈕
     private lazy var likeButton: UIButton = {
         var btn = UIButton()
@@ -57,7 +57,7 @@ class TravelDetailVC: UIViewController {
         btn.addTarget(self, action: #selector(didClickLikeButton), for: .touchUpInside)
         return btn
     }()
-
+    
     /// 分享按鈕
     private lazy var shareButton: UIButton = {
         var btn = UIButton()
@@ -66,7 +66,7 @@ class TravelDetailVC: UIViewController {
         btn.addTarget(self, action: #selector(didClickShareButton), for: .touchUpInside)
         return btn
     }()
-
+    
     /// 更多按鈕
     private lazy var moreButton: UIButton = {
         var btn = UIButton()
@@ -75,27 +75,27 @@ class TravelDetailVC: UIViewController {
         btn.addTarget(self, action: #selector(didClickMoreButton), for: .touchUpInside)
         return btn
     }()
-
+    
     /// 預設 header 高度
     private lazy var headerHeight: CGFloat = {
         var height = 230.0
         return height
     }()
-
+    
     private lazy var titleLabel: UILabel = {
         var label = UILabel()
         label.textColor = .white
         label.font = .systemFont(ofSize: 22, weight: .bold)
         return label
     }()
-
+    
     /// 依照星星數量決定圖片
     private lazy var starsCountImageView: UIImageView = {
         var imageView = UIImageView()
         imageView.backgroundColor = .green
         return imageView
     }()
-
+    
     /// 顯示星星數量文字
     private lazy var starsCountLabel: UILabel = {
         var label = UILabel()
@@ -103,7 +103,7 @@ class TravelDetailVC: UIViewController {
         label.textColor = .white
         return label
     }()
-
+    
     /// 更多圖片按鈕
     private lazy var moreImageButton: UIButton = {
         var btn = UIButton()
@@ -116,34 +116,34 @@ class TravelDetailVC: UIViewController {
         btn.addTarget(self, action: #selector(didClickMoreImageButton), for: .touchUpInside)
         return btn
     }()
-
+    
     init(travelItem: DisplayBusiness) {
         self.viewModel = TravelDetailVM(travelItem: travelItem)
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - 生命週期
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupHeaderDetail()
         navigationController?.setNavigationBarHidden(true, animated: false)
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: false)
     }
-
+    
     private func setupUI() {
         view.addSubview(detailTableView)
         headerImageView.addSubview(backButton)
@@ -155,54 +155,54 @@ class TravelDetailVC: UIViewController {
         headerImageView.addSubview(starsCountLabel)
         headerImageView.addSubview(titleLabel)
         headerImageView.addSubview(moreImageButton)
-
+        
         detailTableView.snp.makeConstraints { make in
             make.top.bottom.leading.trailing.equalToSuperview()
         }
-
+        
         backButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(30)
             make.leading.equalToSuperview().offset(25)
         }
-
+        
         moreButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(30)
             make.trailing.equalToSuperview().offset(-25)
         }
-
+        
         shareButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(30)
             make.trailing.equalTo(moreButton.snp.leading).offset(-20)
         }
-
+        
         likeButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(30)
             make.trailing.equalTo(shareButton.snp.leading).offset(-20)
         }
-
+        
         starsCountImageView.snp.makeConstraints { make in
             make.bottom.equalToSuperview().offset(-20)
             make.leading.equalToSuperview().offset(30)
             make.width.equalToSuperview().multipliedBy(0.3)
             make.height.equalTo(25)
         }
-
+        
         starsCountLabel.snp.makeConstraints { make in
             make.bottom.equalToSuperview().offset(-20)
             make.leading.equalTo(starsCountImageView.snp.trailing).offset(10)
         }
-
+        
         titleLabel.snp.makeConstraints { make in
             make.bottom.equalTo(starsCountImageView.snp.top).offset(-12)
             make.leading.equalToSuperview().offset(30)
         }
-
+        
         moreImageButton.snp.makeConstraints { make in
             make.bottom.equalToSuperview().offset(-20)
             make.trailing.equalToSuperview().offset(-40)
         }
     }
-
+    
     /// 設定 header 區域資料
     private func setupHeaderDetail() {
         guard let travelItem = viewModel?.travelItem else { return }
@@ -210,7 +210,7 @@ class TravelDetailVC: UIViewController {
         starsCountLabel.text = "\(travelItem.rating)"
         setLikeButtonImage(isFavorite: travelItem.isFavorites)
     }
-
+    
     /// 判斷收藏按鈕圖片狀態
     private func setLikeButtonImage(isFavorite: Bool) {
         if isFavorite {
@@ -218,31 +218,31 @@ class TravelDetailVC: UIViewController {
         } else {
             likeButton.setImage(UIImage(systemName: "bookmark"), for: .normal)
         }
-
+        
     }
-
+    
     // MARK: - action
-
+    
     /// 返回按鈕
     @objc private func didClickBackButton() {
         navigationController?.popViewController(animated: true)
     }
-
+    
     /// 更多按鈕
     @objc private func didClickMoreButton() {
         // TODO: 更多按鈕動作
     }
-
+    
     /// 分享按鈕
     @objc private func didClickShareButton() {
         // TODO: 分享按鈕動作
     }
-
+    
     /// 喜歡按鈕
     @objc private func didClickLikeButton() {
         // TODO: 喜歡按鈕動作
     }
-
+    
     /// 更多圖片按鈕
     @objc private func didClickMoreImageButton() {
         // TODO: 更多圖片按鈕動作
@@ -252,7 +252,7 @@ class TravelDetailVC: UIViewController {
 // MARK: - tableView
 
 extension TravelDetailVC: UITableViewDelegate, UITableViewDataSource {
-
+    
     private func registerCell(tableView: UITableView) {
         tableView.register(PlaceActionCell.self, forCellReuseIdentifier: "\(PlaceActionCell.self)")
         tableView.register(BusinessHoursCell.self, forCellReuseIdentifier: "\(BusinessHoursCell.self)")
@@ -262,69 +262,69 @@ extension TravelDetailVC: UITableViewDelegate, UITableViewDataSource {
     
     private func configureCell(for sectionType: TravelDetailSectionType, indexPath: IndexPath, tableView: UITableView) -> UITableViewCell {
         switch sectionType {
-            case .topArea:
+        case .topArea:
+            return UITableViewCell()
+            
+        case .placeAction:
+            guard let placeActionCell = tableView.dequeueReusableCell(withIdentifier: "\(PlaceActionCell.self)", for: indexPath) as? PlaceActionCell else {
                 return UITableViewCell()
-
-            case .placeAction:
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(PlaceActionCell.self)", for: indexPath) as? PlaceActionCell else {
-                    return UITableViewCell()
-                }
-
-                return cell
-
-            case .information:
-                return configureInformationCell(at: indexPath, tableView: tableView)
+            }
+            
+            return placeActionCell
+            
+        case .information:
+            return configureInformationCell(at: indexPath, tableView: tableView)
         }
     }
-
+    
     /// cell 增加方式：增加 InformationCellType case 
     /// 加入新 cell 時，numberOfRowsInSection 要增加數量
     private func configureInformationCell(at indexPath: IndexPath, tableView: UITableView) -> UITableViewCell {
         guard let cellType = InformationCellType(rawValue: indexPath.row) else {
             return UITableViewCell()
         }
-
+        
         switch cellType {
-            case .businessHours:
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BusinessHoursCell.self)", for: indexPath) as? BusinessHoursCell else {
-                    return UITableViewCell()
-                }
-
-                return cell
-            case .phone:
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(PhoneCell.self)", for: indexPath) as? PhoneCell else {
-                    return UITableViewCell()
-                }
-
-                return cell
-            case .map:
-                guard let mapCell = tableView.dequeueReusableCell(withIdentifier: "\(MapCell.self)", for: indexPath) as? MapCell else {
-                    return UITableViewCell()
-                }
-
-                let userLat = 1.2938
-                let userLon = 103.841114
-
-                let destinationLat = 1.284066
-                let destinationLon = 103.841114
-
-                // TODO: 座標等確定後接上實際座標
-                viewModel?.calculateDistanceAndETA(userLat: userLat, userLon: userLon,
-                                                   destinationLat: destinationLat, destinationLon: destinationLon, completion: { distance, travelTime, error in
-                    guard let distance = distance,
-                          let travelTime = travelTime,
-                          let address = self.viewModel?.assembleAddress() else { return }
-
-                    mapCell.convertCell(userLat: userLat, userLon: userLon,
-                                        destinationLat: destinationLat, destinationLon: destinationLon,
-                                        navigateTime: "\(travelTime)",
-                                        distance: "\(distance)",
-                                        address: address)
-                })
-                return mapCell
+        case .businessHours:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BusinessHoursCell.self)", for: indexPath) as? BusinessHoursCell else {
+                return UITableViewCell()
+            }
+            
+            return cell
+        case .phone:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(PhoneCell.self)", for: indexPath) as? PhoneCell else {
+                return UITableViewCell()
+            }
+            
+            return cell
+        case .map:
+            guard let mapCell = tableView.dequeueReusableCell(withIdentifier: "\(MapCell.self)", for: indexPath) as? MapCell else {
+                return UITableViewCell()
+            }
+            
+            let userLat = 1.2938
+            let userLon = 103.841114
+            
+            let destinationLat = 1.284066
+            let destinationLon = 103.841114
+            
+            // TODO: 座標等確定後接上實際座標
+            viewModel?.calculateDistanceAndETA(userLat: userLat, userLon: userLon,
+                                               destinationLat: destinationLat, destinationLon: destinationLon, completion: { distance, travelTime, error in
+                guard let distance = distance,
+                      let travelTime = travelTime,
+                      let address = self.viewModel?.assembleAddress() else { return }
+                
+                mapCell.convertCell(userLat: userLat, userLon: userLon,
+                                    destinationLat: destinationLat, destinationLon: destinationLon,
+                                    navigateTime: "\(travelTime)",
+                                    distance: "\(distance)",
+                                    address: address)
+            })
+            return mapCell
         }
     }
-
+    
     /// 控制點選到的 cell
     private func configureSelectInformationCell(indexPath: IndexPath) {
         guard let section = TravelDetailSectionType(rawValue: indexPath.section),
@@ -332,81 +332,81 @@ extension TravelDetailVC: UITableViewDelegate, UITableViewDataSource {
               let cellType = InformationCellType(rawValue: indexPath.row) else {
             return
         }
-
+        
         switch cellType {
-            case .businessHours:
-                return
-            case .phone:
-                callPhoneNumber()
-            case .map:
-                return
+        case .businessHours:
+            return
+        case .phone:
+            callPhoneNumber()
+        case .map:
+            return
         }
-
+        
     }
-
+    
     /// 撥打電話
     private func callPhoneNumber() {
         guard let phoneNumber = viewModel?.travelItem?.phone,
               let phoneURL = URL(string: "tel://\(phoneNumber)") else { return }
-
+        
         if UIApplication.shared.canOpenURL(phoneURL) {
             UIApplication.shared.open(phoneURL)
         } else {
             Logger.errorLog(message: "無法撥打電話")
         }
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
         configureSelectInformationCell(indexPath: indexPath)
     }
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return TravelDetailSectionType.allCases.count
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         guard let sectionType = TravelDetailSectionType(rawValue: section) else {
             return 0
         }
-
+        
         // 返回每個 section 需要的 cell 數量
         switch sectionType {
-            case .topArea:
-                return 0
-            case .placeAction:
-                return 1
-            case .information:
-                return 3
+        case .topArea:
+            return 0
+        case .placeAction:
+            return 1
+        case .information:
+            return 3
         }
-
+        
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         guard let sectionType = TravelDetailSectionType(rawValue: indexPath.section) else {
             return UITableViewCell()
         }
-
+        
         return configureCell(for: sectionType, indexPath: indexPath, tableView: tableView)
-
+        
     }
-
+    
     /// 依照 section 返回指定 headerView 高度
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-
+        
         guard let sectionType = TravelDetailSectionType(rawValue: section) else {
             return 0
         }
-
+        
         switch sectionType {
-            case .topArea:
-                return headerHeight
-            case .placeAction:
-                return 0
-            case .information:
-                return 30
+        case .topArea:
+            return headerHeight
+        case .placeAction:
+            return 0
+        case .information:
+            return 30
         }
     }
 }
--- HelloTravel/ViewController/TravelDetailVC/TravelDetailVM.swift
+++ HelloTravel/ViewController/TravelDetailVC/TravelDetailVM.swift
@@ -9,63 +9,63 @@ import Foundation
 import MapKit
 
 class TravelDetailVM {
-
+    
     /// 顯示用資料
     private(set) var travelItem: DisplayBusiness?
-
+    
     init(travelItem: DisplayBusiness) {
         self.travelItem = travelItem
     }
-
+    
     func getBusinessItem() -> DisplayBusiness? {
         return travelItem
     }
-
+    
     /// 計算路線
     func calculateDistanceAndETA(userLat: Double, userLon: Double, destinationLat: Double, destinationLon: Double, completion: @escaping (_ distance: CLLocationDistance?, _ travelTime: String?, _ error: Error?) -> Void) {
-
+        
         let request = MKDirections.Request()
         let userLocation = CLLocationCoordinate2D(latitude: userLat, longitude: userLon)
         let destination = CLLocationCoordinate2D(latitude: destinationLat, longitude: destinationLon)
-
+        
         request.source = MKMapItem(placemark: MKPlacemark(coordinate: userLocation))
         request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destination))
         // 交通方式
         request.transportType = .automobile
-
+        
         let directions = MKDirections(request: request)
         directions.calculate { response, error in
-
+            
             guard let route = response?.routes.first else {
                 completion(nil, nil, error)
                 return
             }
-
+            
             // 距離（米）轉 公里
             let distance = route.distance / 1000
             // 時間(秒)
             let travelTime = self.formattedTimeString(time: route.expectedTravelTime)
-
+            
             completion(distance, travelTime, nil)
         }
     }
-
+    
     /// 將秒數轉換成小時、分鐘
     private func formattedTimeString(time: TimeInterval?) -> String? {
-
+        
         guard let time = time else { return ""}
-
+        
         let hours = Int(time) / 3600
         let minutes = (Int(time) % 3600) / 60
-
+        
         if hours > 0 {
             return "\(hours)小時\(minutes)分鐘"
         } else {
             return "\(minutes)分鐘"
         }
-
+        
     }
-
+    
     /// 取得地址
     func assembleAddress() -> String {
         var address = ""
@@ -73,8 +73,8 @@ class TravelDetailVM {
         for addressItem in item.location.displayAddress {
             address += addressItem
         }
-
+        
         return address
     }
-
+    
 }
