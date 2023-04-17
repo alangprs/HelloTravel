e1d001098b3f9b12bda627526f52f03f5ea00444
Will_Weng <alangprs1@gmail.com>
2023-04-17 11:52:57 +0800

    add: 自定義顏色

---
+++ HelloTravel/Extension/UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor.swift
+//  HelloTravel
+//
+//  Created by cm0768 on 2023/4/17.
+//
+
+import UIKit
+
+extension UIColor {
+
+    /// 淡藍色
+    class var myLightBlue: UIColor {
+        return UIColor(red: 76 / 255, green: 124 / 255, blue: 198 / 255, alpha: 1)
+    }
+
+    /// 背景 - 淡藍色
+    class var bgLightBlue: UIColor {
+        return UIColor(red: 176 / 255, green: 224 / 255, blue: 255 / 255, alpha: 0.8)
+    }
+
+}
